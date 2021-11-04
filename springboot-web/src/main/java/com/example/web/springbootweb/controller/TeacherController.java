92af9d1ed50b2a498af516a676010c0e08641e37
yxr2333 <54535282@qq.com>
2021-11-04 22:28:48 +0800

    🛠️Add: add SpringBootWeb modules

---
+++ springboot-web/src/main/java/com/example/web/springbootweb/controller/TeacherController.java
@@ -0,0 +1,10 @@
+package com.example.web.springbootweb.controller;
+
+/**
+ * Created by Intellij IDEA
+ *
+ * @Author: Xinrui Yu
+ * @Date: Created in 22:26 2021/11/4
+ */
+public class TeacherController {
+}
